7d19f544f85cc5aa445a32cf5913d1648409b633
skoeba_steve <janssenman@gmail.com>
2013-11-07 16:12:37 +0100

    loading data from xml with js into table

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	//GET ALL TODO'S FORM XML FILE
 	$.ajax({
 		type: "GET",
-		url: "../data/todo.xml",
+		url: "data/todo.xml",
 		dataType: "xml",
 		success: function(xml) {
 			//USE EACH FUNCTION TO LOOP THROUGH ALL THE TODOS IN THE XML FILE
@@ -12,7 +12,7 @@ $(document).ready(function(){
 				var date = $(this).find('date').text();
 				var done = $(this).find('done').text();
 				//USE appendTo FOR PUTTING THE FOUND DATA AFTER THE LAST ONE
-				$('<tr><td>'+name+'</td><td'+date+'></td><td>'+done+'</td></tr>').appendTo('#todo');
+				$('<tr><td>'+name+'</td><td>'+date+'</td><td>'+done+'</td></tr>').appendTo('#todo');
 			});
 		}
 	});
